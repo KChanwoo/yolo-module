3ee62c09c873d304d432513189f7be6dd698d5d9
chanwoo kwon <arknell@yonsei.ac.kr>
2020-08-29 02:13:57 +0900

    change package

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from core.builder import Yolo
+from yolov4_module.builder import Yolo
 
 yolo = Yolo("./", "./data/classes/graph.names", "./data/dataset/graph.txt", "./data/dataset/graph_test.txt", None)
 
